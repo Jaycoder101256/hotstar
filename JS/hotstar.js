4f5944efc4de4dd2fae96e2259b61048ddb7ec41
Jay Dhamija <jaydhamija@email.com>
2023-07-19 03:27:19 +0530

    OK

---
+++ JS/hotstar.js
@@ -0,0 +1,116 @@
+let movies = [{
+        name: "Falcon and Winter Soldier",
+        des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        image: "../images/images/slider 2.png"
+    },
+    {
+        name: "Loki",
+        des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        image: "../images/images/slider 1.png"
+    },
+    {
+        name: "Wanda and Vision",
+        des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        image: "../images/images/slider 3.png"
+    },
+    {
+        name: "Raya and the Last Dragon",
+        des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        image: "../images/images/slider 4.png"
+    },
+    {
+        name: "Luca",
+        des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        image: "../images/images/slider 5.png"
+    }
+];
+
+const carousel = document.querySelector('.carousel');
+let sliders = [];
+
+let slideIndex = 0; // to track current slide index.
+
+
+const createSlide = () => {
+    if (slideIndex >= movies.length) {
+        slideIndex = 0;
+    }
+
+    // creating DOM element
+    let slide = document.createElement('div');
+    let imgElement = document.createElement('img');
+    let content = document.createElement('div');
+    let h1 = document.createElement('h1');
+    let p = document.createElement('p');
+
+    // attaching all elements
+    imgElement.appendChild(document.createTextNode(''));
+    h1.appendChild(document.createTextNode(movies[slideIndex].name));
+    p.appendChild(document.createTextNode(movies[slideIndex].des));
+    content.appendChild(h1);
+    content.appendChild(p);
+    slide.appendChild(content);
+    slide.appendChild(imgElement);
+    carousel.appendChild(slide);
+
+
+
+    // setting up image
+    imgElement.src = movies[slideIndex].image;
+    slideIndex++;
+
+    // setting elements classname
+    slide.className = 'slider';
+    content.className = 'slide-content';
+    h1.className = 'movie-title';
+    p.className = 'movie-des';
+
+    sliders.push(slide);
+
+
+
+    if (sliders.length) {
+        sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${30 * (sliders.length - 2)}px)`;
+    }
+}
+
+for (let i = 0; i < 3; i++) {
+    createSlide();
+}
+
+setInterval(() => {
+    createSlide();
+}, 3000);
+
+
+let cardContainers = [...document.querySelectorAll('.card-container')];
+let preBtns = [...document.querySelectorAll('.pre-btn')];
+let nxtBtns = [...document.querySelectorAll('.nxt-btn')];
+
+cardContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtns[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth - 200;
+    })
+
+    preBtns[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth + 200;
+    })
+})
+
+
+const videoCards = document.querySelectorAll('.video-card');
+
+videoCards.forEach(item => {
+    item.addEventListener('mouseover', () => {
+        let video = item.children[1];
+        video.play();
+    });
+
+    item.addEventListener('mouseleave', () => {
+        let video = item.children[1];
+        video.pause();
+    });
+});
